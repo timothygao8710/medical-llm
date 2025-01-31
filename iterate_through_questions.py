53816556dbe112996bbc83717c0cc9df854fd1ff
Timothy Gao <timothygao8710@gmail.com>
2025-01-31 11:36:49 -0800

    add r1

--- iterate_through_questions.py
+++ iterate_through_questions.py
@@ -1,3 +1,4 @@
+import re
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import pandas as pd
@@ -6,30 +7,16 @@ import json
 import random
 import torch.nn.functional as F
 import os
-from question_loader import get_correct_answer, get_row_query, get_row_query_cot
+from question_loader import get_correct_answer, get_row_query, get_row_query_cot, get_data_len
 import pickle
 from utils import get_entropy_from_probabilities, dump_data
-from LLM import get_next_token_fast, EOS_TOKEN, generate_response
+from LLM import load_model, generate
 import numpy as np
 
-##### SETTINGS #####
-cache_dir = '/tmp'
-model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-# model_name = "medalpaca/medalpaca-7b"
-possible_outputs = ["A", "B", "C", "D", "E", "F", "G", "H"]
-batch_size = 8
-# redownload = False
-redownload = True
-data_outpath = './LLM_reults/cot'
-######################
+load_model("deepseek-ai/DeepSeek-R1-Distill-Llama-8B")  # Must be explicitly called before using other functions
 
-if redownload:
-    model_cache_path = os.path.join(cache_dir, model_name)
-    if os.path.exists(model_cache_path):
-        os.rmdir(model_cache_path)
-
-tot_questions = 20
-# tot_questions = get_data_len()
+# tot_questions = 20
+tot_questions = get_data_len()
 print(tot_questions)
 
 res = []
@@ -37,28 +24,30 @@ correct_count = 0
 
 temp = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7}
 
+def extract_answer(llm_response):
+    matches = re.findall(r"<box>(.*?)</box>", llm_response)
+    return matches[-1] if matches else None
+
 with tqdm(total=tot_questions) as pbar:
     for row in range(tot_questions):
         cor_ans = get_correct_answer(row)
         query = get_row_query_cot(row)
-        response, entropies = generate_response(query)
+        # response, entropies = generate_response(query)
+        response, entropies = generate(query, max_length=3000), []
         
-        print(query, response, entropies)
+        model_ans = extract_answer(response)
         
-        temp = response.split('Therefore, the final answer is: ')
-        if(len(temp) > 1):
-            model_ans = temp[1][0]
-        else:
-            model_ans = None
-        
-        print(model_ans)
-        print('=' * 100)
+        print("0"*100)
+        print(response)
+        print("0"*100)
         
         correct_count += 1 if model_ans == cor_ans else 0
         pbar.set_postfix({'Correct %': f'{(correct_count / (row + 1)) * 100:.2f}%'})
         pbar.update(1)
-
-        print(row, model_ans == cor_ans, np.mean(entropies), model_ans, cor_ans)
+        
+        print(row, model_ans == cor_ans, "model ans", model_ans, "cor ans", cor_ans)
+        print('=' * 100)
+        
         res.append({
                 "row": row,
                 "avg_entropy": np.mean(entropies),
@@ -68,7 +57,7 @@ with tqdm(total=tot_questions) as pbar:
                 "model_ans":model_ans
             })
 
-    if (row + 1) % 100 == 0:
-        dump_data(res, data_outpath)        
+#     if (row + 1) % 100 == 0:
+#         dump_data(res, data_outpath)        
 
-dump_data(res, data_outpath)
+# dump_data(res, data_outpath)
